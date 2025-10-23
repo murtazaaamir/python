d647a090ca32e66dafe2e4f04c9bf0cf09131f0e
murtazaaamir <murtazaaamir62@gmail.com>
2025-10-23 05:49:51 -0700

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,28 @@
+name = "Penguin"
+age = 15
+is_student = True
+weight = 38.5
+
+
+print("Name :", name)
+print("data type of name is", type(name))
+print("Age :", age)
+print("data type of age is",type(age))
+print("is_student :", is_student)
+print("Data type of is_student is", type(is_student))
+print("weight :", weight)
+print("data type of weight is", type(weight))
+
+print("\n After Type Casting....")
+
+age = str(age)
+
+print(age)
+
+print("Data Type of age is", type(age))
+
+weight = int(weight)
+print(weight)
+print("data type of weight is", type(weight))
+
+
---
+++ opretors.py
@@ -0,0 +1,15 @@
+num1 = 45
+num2 = 3
+
+
+print("number 1", num1)
+print("number 2", num2)
+print("addition:", num1+num2)
+print("difference:",num1-num2)
+print("product :", num1*num2)
+print("division :",num1/num2)
+print("floor division ",num1//num2)
+print("",)
+print("",)
+print("",)
+print("",)
